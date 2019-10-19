24eb53d8891d8939bc7e9a71fb0d753a5967c522
John Lewis <johnlewis@MacBook-Pro.local>
2019-10-18 19:44:35 -0600

    initial

---
+++ server/index.js
@@ -0,0 +1,14 @@
+const express = require('express')
+const ctrl = require('./controllers/books_controller.js')
+
+const app = express ()
+
+app.use(express.json())
+
+app.get('/api/books', ctrl.read)
+app.post('/api/books',ctrl.create)
+app.put('/api/books/:id',)
+
+const PORT = 4000
+app.listen(PORT, () => 
+{console.log(`${PORT} BOTTLES OF BEER ON THE WALL`)})
